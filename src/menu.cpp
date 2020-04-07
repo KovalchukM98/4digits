1e1f686762d28347eaf7c85051e02354793393fb
KovalchukM98 <babur1189@gmail.com>
2020-04-07 15:18:51 +0700

    menu.cpp: void join

--- src/menu.cpp
+++ src/menu.cpp
@@ -15,10 +15,38 @@ public:
 	~Menu(){}
 
 	void join(){
-		cout << "1 новая игра" <<endl;
+		cout << "1 новая игра" << endl;
+		cout << "2 таблица рекордов" << endl;
+		cout << "3 о программе" << endl;
+		cout << "4 выход" << endl;
+		int key = 0;
+		bool flag = false;
+		while(!flag){
+			cin >> key;
+			switch(key){
+				case 1 :
+				start();
+				break;
+				case 2 :
+				show_leader_board();
+				break;
+				case 3:
+				about();
+				break;
+				case 4:
+				flag = true;
+			}
+		}
 	}
+
+	void start(){}
+
+	void show_leader_board(){}
+
+	void about(){}
+
 private:
 	Game *game;
-}
+};
 
 #endif
