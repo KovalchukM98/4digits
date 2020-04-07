822e38888f21a1d34179e550f0ec0e00ace3adda
Restov <romankotovv@gmail.com>
2020-04-07 15:12:08 +0700

    Add makerand

--- src/sequence.cpp
+++ src/sequence.cpp
@@ -9,10 +9,37 @@ public:
         trueseq = new char[4];
     }
 
-    ~Sequence() {}
+    ~Sequence()
+    {
+        delete[] trueseq;
+    }
 
 private:
     char *trueseq;
+    void makerand(char trueseq[])
+    {
+        srand(time(nullptr));
+        bool is;
+        int element;
+        for (int i = 0; i < n;)
+        {
+            is = false;
+            element = rand() % 9;
+            for (int j = 0; j < i; ++j)
+            {
+                if (trueseq[j] == (element + '0'))
+                {
+                    there = true;
+                    break;
+                }
+            }
+            if (!there)
+            {
+                trueseq[i] = element + '0';
+                ++i;
+            }
+        }
+    }
 }
 
 #endif
