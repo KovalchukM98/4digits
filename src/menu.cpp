696a347a9d59a05b44db023745948b6be798c722
KovalchukM98 <babur1189@gmail.com>
2020-04-07 15:17:56 +0700

    created all classes

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,11 +1,24 @@
 #ifndef MENU
 #define MENU
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "game.cpp"
+
+using namespace std;
+
 class Menu{
 public:
+	Menu(){}
 
+	~Menu(){}
+
+	void join(){
+		cout << "1 новая игра" <<endl;
+	}
 private:
-	
+	Game *game;
 }
 
 #endif
