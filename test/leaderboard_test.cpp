a9d8322629891ebce68a307a7888498e03502384
Restov <romankotovv@gmail.com>
2020-04-29 18:26:36 +0700

    Format test

--- test/leaderboard_test.cpp
+++ test/leaderboard_test.cpp
@@ -2,20 +2,22 @@
 #include <leaderboard.h>
 #include <string>
 
-TEST(LEADERBOARD, isNameValid) {
-  std::string teststring;
-  Leaderboard testboard;
-  teststring = "Name";
-  ASSERT_EQ(true, testboard.is_name_valid(teststring));
-  teststring = "Anything123";
-  ASSERT_EQ(true, testboard.is_name_valid(teststring));
-  teststring = "123";
-  ASSERT_EQ(true, testboard.is_name_valid(teststring));
+TEST(LEADERBOARD, isNameValid)
+{
+    std::string teststring;
+    Leaderboard testboard;
+    teststring = "Name";
+    ASSERT_EQ(true, testboard.is_name_valid(teststring));
+    teststring = "Anything123";
+    ASSERT_EQ(true, testboard.is_name_valid(teststring));
+    teststring = "123";
+    ASSERT_EQ(true, testboard.is_name_valid(teststring));
 }
 
-TEST(LEADERBOARD, isNameInvalid) {
-  std::string teststring;
-  Leaderboard testboard;
-  teststring = " ";
-  ASSERT_EQ(false, testboard.is_name_valid(teststring));
+TEST(LEADERBOARD, isNameInvalid)
+{
+    std::string teststring;
+    Leaderboard testboard;
+    teststring = " ";
+    ASSERT_EQ(false, testboard.is_name_valid(teststring));
 }
