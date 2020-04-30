2039de04e12120a45118b7b7386019ba4509504d
KovalchukM98 <babur1189@gmail.com>
2020-04-30 16:49:50 +0700

    count_bulls_and_cows test

--- test/sequence_test.cpp
+++ test/sequence_test.cpp
@@ -11,6 +11,19 @@ TEST(SEQUENCE, count_bulls_and_cows)
 	std::vector<char> alphabet = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	Sequence sec(alphabet, 4);
 	std::string answer = sec.get_answer();
+	std::pair<int, int> res;
+	ASSERT_EQ(sec.count_bulls_and_cows(answer).first, 4);
+	ASSERT_EQ(sec.count_bulls_and_cows(answer).second, 0);
+	char tmp = answer[0];
+	answer[0] = answer[1];
+	answer[1] = tmp;
+	ASSERT_EQ(sec.count_bulls_and_cows(answer).first, 2);
+	ASSERT_EQ(sec.count_bulls_and_cows(answer).second, 2);
+	tmp = answer[2];
+	answer[2] = answer[3];
+	answer[3] = tmp;
+	ASSERT_EQ(sec.count_bulls_and_cows(answer).first, 0);
+	ASSERT_EQ(sec.count_bulls_and_cows(answer).second, 4);
 }
 
 #endif
