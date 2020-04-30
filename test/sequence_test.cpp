1477b21389a59e9af84afcd5521c75df584f5d4c
Restov <romankotovv@gmail.com>
2020-04-30 16:45:46 +0700

    format

--- test/sequence_test.cpp
+++ test/sequence_test.cpp
@@ -8,9 +8,10 @@
 
 TEST(SEQUENCE, count_bulls_and_cows)
 {
-	std::vector<char> alphabet = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-	Sequence sec(alphabet, 4);
-	std::string answer = sec.get_answer();
+    std::vector<char> alphabet
+            = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    Sequence sec(alphabet, 4);
+    std::string answer = sec.get_answer();
 }
 
 #endif
