258bde96cffe596f352c513d1c8f0d994a17de20
KovalchukM98 <babur1189@gmail.com>
2020-04-30 16:40:20 +0700

    sequence first test

--- test/sequence_test.cpp
+++ test/sequence_test.cpp
@@ -4,10 +4,13 @@
 #include "sequence.h"
 #include <gtest/gtest.h>
 #include <string>
+#include <vector>
 
 TEST(SEQUENCE, count_bulls_and_cows)
 {
-
+	std::vector<char> alphabet = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	Sequence sec(alphabet, 4);
+	std::string answer = sec.get_answer();
 }
 
 #endif
